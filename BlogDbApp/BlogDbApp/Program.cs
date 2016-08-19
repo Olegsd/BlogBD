2c457bd8e0e606fe5c9cc5a54d746579b0308dbf
OlegDimitrov <olegsdimitrov@gmail.com>
2016-08-19 15:18:31 +0300

    Some request

--- BlogDbApp/BlogDbApp/Program.cs
+++ BlogDbApp/BlogDbApp/Program.cs
@@ -6,5 +6,20 @@ using System.Threading.Tasks;
 
 namespace BlogDbApp
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new BlogDbContext();
+            foreach (var p in db.Posts)
+            {
+                Console.WriteLine(p.ID + " " + p.Title);
+                foreach (var c in p.Comments)
+                {
+                    Console.WriteLine($"\t {c.ID} {c.Text}");
+                }
+            }
+        }
 
+    }
 }
